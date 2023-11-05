5710fa2bdfc158c686e83ea8ea30ec73005c63f4
joshuak29 <rockjosh454@gmail.com>
2023-11-05 16:50:50 +0200

    modified smallUser

--- beAt/src/components/shared/SmallUser.vue
+++ beAt/src/components/shared/SmallUser.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="w-full px-4 py-2 flex flex-row border-b-2 border-gray-300">
     <!-- Profile img -->
-    <router-link to="/user"><img src="" class="w-16 aspect-square rounded-full bg-gray-500" /></router-link>
+    <router-link to="/user"><img src="" class="w-14 aspect-square rounded-full bg-gray-500" /></router-link>
 
     <!-- name and username -->
-    <router-link to="/user">
+    <router-link to="/user" class="grow">
       <div class="flex flex-col grow justify-center pl-4">
-        <div class="text-lg">Rukundo Josue</div>
-        <div class="opacity-60">@dark_jnr</div>
+        <div>Rukundo Josue</div>
+        <div class="opacity-60 text-sm">@dark_jnr</div>
       </div>
     </router-link>
 
     <!-- add friend icon -->
-    <div class="grow flex items-center justify-center" @click="follow">
-      <font-awesome-icon icon="user-minus" class="text-2xl text-red-500" v-if="following"/>
-      <font-awesome-icon icon="user-plus" class="text-2xl" v-else/>
+    <div class="flex items-center justify-center px-4" @click="follow">
+      <font-awesome-icon icon="user-minus" class="text-xl text-red-500" v-if="following"/>
+      <font-awesome-icon icon="user-plus" class="text-xl" v-else/>
     </div>
   </div>
 </template>
